bd122a34059dda389aeee2ed75d1a85041065368
Samuel Wei <samuelwwei@163.com>
2024-03-16 18:07:27 +1300

    initial post

---
+++ src/component/Posts/Post.js
@@ -0,0 +1,15 @@
+const Post =({post:{title, body, imgUrl, author}, index}) =>{
+    // console.log(`${title}, ${body}, ${imgUrl}, ${author}`);
+    return(
+        <div className="post-content">
+            <h1 className="post-head">{title}</h1>
+            <img className="post-img" src={imgUrl} alt="post"></img>
+            <p>{body}</p>
+            <div className="info">
+                <h4>Written by: {author}</h4>
+            </div>
+        </div>
+    )
+};
+
+export default Post;
